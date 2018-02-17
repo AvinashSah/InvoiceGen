74f84e6696602202c5b91fc291aaa3a5b9724314
Avinash Kumar <avi.kumar.sah@live.com>
2018-02-17 20:11:33 +0530

    data model updated

--- InvoiceGen.DAL/DAL_Products.cs
+++ InvoiceGen.DAL/DAL_Products.cs
@@ -39,16 +39,18 @@ namespace InvoiceGen.DAL
                 }
                 catch (DbEntityValidationException e)
                 {
-                    //foreach (var eve in e.EntityValidationErrors)
-                    //{
-                    //    //Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
-                    //    //    eve.Entry.Entity.GetType().Name, eve.Entry.State);
-                    //    foreach (var ve in eve.ValidationErrors)
-                    //    {
-                    //        //Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
-                    //        //ve.PropertyName, ve.ErrorMessage);
-                    //    }
-                    //}
+                    foreach (var eve in e.EntityValidationErrors)
+                    {
+                        foreach (var ve in eve.ValidationErrors)
+                        {
+                            throw new Exception(string.Format("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
                 }
                 return listProduct;
             }
